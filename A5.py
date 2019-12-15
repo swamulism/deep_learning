5d3318096fceade67811cf4e5ab43cbcbece7971
samuel <swamulism@gmail.com>
2019-12-15 13:09:03 -0700

    Part 2

--- A5.py
+++ A5.py
@@ -10,11 +10,11 @@ from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
 from keras.applications.vgg19 import preprocess_input
 from keras.models import Model
+from sklearn.model_selection import train_test_split
 import os
 import re
-
 import numpy as np
-
+import matplotlib.pyplot as plt
 from PIL import Image
 
 
@@ -62,11 +62,14 @@ def getVGGFeatures(img, layerName):
 
 	return internalFeatures
 
+
 def cropImage(image, x1, y1, x2, y2):
 	return image.crop((x1, y1, x2, y2))
 
+
 def standardizeImage(image, x, y):
 	return image.resize((x, y))
+
 
 def preProcessImages(images=None):
 	directory = "cropped"
@@ -89,36 +92,43 @@ def preProcessImages(images=None):
 		except:
 			print(f"{filename} fail")
 
+
 def visualizeWeight():
 	#You can change these parameters if you need to
 	utils.raiseNotDefined()
 
+
 def trainFaceClassifier(preProcessedImages, labels):
-	preProcessedImages = map(lambda x: x.convert("L"), preProcessedImages)
+	# turn images into greyscale
+	preProcessedImages = np.asarray(list(map(lambda x: np.array(x.convert("L")), preProcessedImages)))
 
-	preProcessedImages, labels = zip(zip(preProcessedImages, labels).shuffle())
-	
-	batch_size = 32
-	n_classes = len(set(labels))
-	epochs = 20
+	# enumerate varible categories from string to int
+	enum_labels = dict((y, x) for x, y in enumerate(set(labels)))
+	labels = np.asarray([enum_labels[x] for x in labels])
 
-	# input image dimensions
+	# set params
+	batch_size = 16
+	n_classes = len(enum_labels)
+	epochs = 40
 	img_rows, img_cols = 60, 60
 
-	# the data, split between train and test sets
-	X_train, X_test = split_data(preProcessedImages, 0.2)
-	X_train, X_valid = split_data(X_train, 0.2)
-
-	y_train, y_test = split_data(labels, 0.2)
-	y_train, y_valid = split_data(y_train, 0.2)
+	# shuffle data and split into train, test, validation
+	X_train, X_test, y_train, y_test = train_test_split(preProcessedImages, labels, test_size=0.2)
+	X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.2)
+	
+	# let's print the shape before we reshape and normalize
+	print("X_train shape", X_train.shape)
+	print("y_train shape", y_train.shape)
+	print("X_test shape", X_test.shape)
+	print("y_test shape", y_test.shape)
 
 	# building the input vector from the 60x60 pixels
 	X_train = X_train.reshape(X_train.shape[0], img_rows * img_cols)
 	X_test = X_test.reshape(X_test.shape[0], img_rows * img_cols)
-	X_valid = X_valid.reshape(X_test.shape[0], img_rows * img_cols)
+	X_valid = X_valid.reshape(X_valid.shape[0], img_rows * img_cols)
 	X_train = X_train.astype('float32')
 	X_test = X_test.astype('float32')
-	X_vaild = X_vaild.astype('float32')
+	X_valid = X_valid.astype('float32')
 
 	# normalizing the data to help with the training
 	X_train /= 255
@@ -136,39 +146,49 @@ def trainFaceClassifier(preProcessedImages, labels):
 	model = Sequential()
 	model.add(Dense(512, input_shape=(img_rows*img_cols,)))
 	model.add(Activation('relu'))                            
-
-	model.add(Dense(10))
+	model.add(Dense(n_classes))
 	model.add(Activation('softmax'))
 
-
 	model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
-
 
 	# training the model and saving metrics in history
 	history = model.fit(X_train, y_train,
 			batch_size=batch_size, epochs=epochs,
 			verbose=2, validation_data=(X_valid, y_valid))
 
-
 	score = model.evaluate(X_test, y_test, verbose=0)
 	print('Test loss:', score[0])
 	print('Test accuracy:', score[1])
 
-	# split into training, validation and test data
+	
+	# plot loss
+	x = range(1, len(history.history["val_loss"]) + 1)
+	fig, ax = plt.subplots(figsize=(12, 8))
+	ax.plot(x, history.history["val_loss"], label="validation loss")
+	ax.plot(x, history.history["loss"] , label="training loss")
+	ax.set_title('Model Loss')
+	ax.set_ylabel('Loss')
+	ax.set_xlabel('Epoch')
+
+	plt.legend()
+
+	ax.yaxis.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.5)
+	fig.savefig('plot1.png')
 
 
 def trainFaceClassifier_VGG(extractedFeatures, labels):
 	utils.raiseNotDefined()
 
-def split_data(data, split):
-	return data[:int(len(data) * (1 - split))], data[int(len(data) * (1 - split)):]
 
 if __name__ == '__main__':
-	print("Your Program Here")
+	print("A5")
 	# Part 1
 	# preProcessImages()
 
 	# Part 2
-	# preProcessedImages = [Image.open(filename) for filename in os.listdir("cropped")]
-	# labels = [re.search(r'^[^\d]*', filename).group() for filename in os.listdir("cropped")]
-	# trainFaceClassifier(preProcessedImages, labels)
+
+	# grabs all files in given directory and gets images and image label (letters up till first digit)
+	directory = "cropped"
+	preProcessedImages = [Image.open(directory + "/" + filename) for filename in os.listdir(directory)]
+	labels = [re.search(r'^[^\d]*', filename).group() for filename in os.listdir(directory)]
+	trainFaceClassifier(preProcessedImages, labels)
